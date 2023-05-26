78be7360a4013fe6749c29bca4f5c52c91241910
Amina <aminabakkali2001@gmail.com>
2023-05-26 18:10:15 +0100

    all done - needs more styling

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ def displayGraph():
         plt.ion()
         plt.show()
         plt.savefig(os.path.join(img_folder, 'graph.png'), format='png')
-        return render_template('DisplayGraph.html')
+        return render_template('DisplayGraph.html', display_result=True)
     else:
-        return render_template('DisplayGraph.html')
+        return render_template('DisplayGraph.html', display_result=False)
 
 
 @app.route('/graph-algorithms/dfs', methods=['GET', 'POST'])
@@ -89,9 +89,9 @@ def dfsAlgo():
 
         plt.savefig(os.path.join(img_folder, 'dfs_graph.png'), format='png')
 
-        return render_template('Dfs.html', dfs_result=dfs_result)
+        return render_template('Dfs.html', dfs_result=dfs_result, display_result=True)
     else:
-        return render_template('Dfs.html')
+        return render_template('Dfs.html', display_result=False)
 
 
 @app.route('/graph-algorithms/bfs', methods=['GET', 'POST'])
@@ -132,9 +132,9 @@ def bfsAlgo():
 
         plt.savefig(os.path.join(img_folder, 'bfs_graph.png'), format='png')
 
-        return render_template('Bfs.html', bfs_result=bfs_result)
+        return render_template('Bfs.html', bfs_result=bfs_result, display_result=True)
     else:
-        return render_template('Bfs.html')
+        return render_template('Bfs.html', display_result=False)
 
 
 @app.route('/graph-algorithms/prim', methods=['GET', 'POST'])
@@ -190,78 +190,9 @@ def primAlgo():
         plt.savefig(os.path.join(
             img_folder, 'prim_graph_after.png'), format='png')
 
-        return render_template('Prim.html', prim_result=prim_result)
+        return render_template('Prim.html', prim_result=prim_result, display_result=True)
     else:
-        return render_template('Prim.html')
-
-
-@app.route('/graph', methods=['GET', 'POST'])
-def graph():
-    if request.method == 'POST':
-        num_nodes = int(request.form['num_nodes'])
-        adj_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
-
-        for i in range(num_nodes):
-            for j in range(num_nodes):
-                adj_matrix[i][j] = int(request.form[f'adj_matrix_{i}_{j}'])
-
-        start_node = int(request.form['start_node'])
-
-        graph = nx.DiGraph(adj_matrix)
-
-        prim = Prim(num_nodes)
-        prim.graph = adj_matrix
-        prim_result = prim.primMST()
-
-        print("prim", prim_result)
-
-        dfs_result = list(map(str, dfs(graph, start_node))
-                          ) if dfs(graph, start_node) else None
-        bfs_result = list(map(str, bfs(graph, start_node))
-                          ) if bfs(graph, start_node) else None
-
-        img_folder = "C:\\Users\\lenovo\\Desktop\\TG GUI Flask\\static\\img"
-        # delete existing images in folder
-        for filename in os.listdir(img_folder):
-            file_path = os.path.join(img_folder, filename)
-            os.unlink(file_path)
-
-        fig = plt.figure(1)
-        fig.clf()
-        plt.title('Graph')
-
-        # Draw the graph with edge labels
-        pos = nx.spring_layout(graph)
-        edge_labels = nx.get_edge_attributes(graph, 'weight')
-        nx.draw(graph, pos, with_labels=True, node_color='lightblue',
-                node_size=500, font_size=10, font_weight='bold')
-        nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
-
-        plt.ion()
-        plt.show()
-
-        plt.savefig(os.path.join(img_folder, 'graph.png'), format='png')
-
-        fig = plt.figure(2)
-        fig.clf()
-        plt.title('Prim\'s Graph')
-        prim_graph = nx.DiGraph()
-        prim_graph.add_edges_from(prim_result.keys())
-        pos = nx.spring_layout(prim_graph)
-        labels = nx.get_edge_attributes(prim_graph, 'weight')
-        nx.draw(prim_graph, pos, with_labels=True)
-        nx.draw_networkx_edge_labels(prim_graph, pos, edge_labels=labels)
-        plt.savefig(os.path.join(img_folder, 'prim_graph.png'), format='png')
-
-        # fig = plt.figure(1)
-        # fig.clf()
-        # plt.title('Graph')
-        # nx.draw(graph, with_labels=True)
-        # plt.savefig(os.path.join(img_folder, 'graph.png'), format='png')
-
-        return render_template('result.html', bfs_result=bfs_result, dfs_result=dfs_result, prim_result=prim_result)
-    else:
-        return render_template('graph.html')
+        return render_template('Prim.html', display_result=False, prim_result={})
 
 
 if __name__ == '__main__':
