74269685ef6d85c510cf932c306c6e4f593afff3
Amina <aminabakkali2001@gmail.com>
2023-05-26 13:36:43 +0100

    navbar done + display a graph

--- app.py
+++ app.py
@@ -14,7 +14,45 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def home():
+    return render_template('index.html')
+
+
+@app.route('/display-graph', methods=['GET', 'POST'])
+def weightGraph():
+    if request.method == 'POST':
+        num_nodes = int(request.form['num_nodes'])
+        adj_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
+        for i in range(num_nodes):
+            for j in range(num_nodes):
+                adj_matrix[i][j] = int(request.form[f'adj_matrix_{i}_{j}'])
+
+        graph = nx.DiGraph(adj_matrix)
+        img_folder = "C:\\Users\\lenovo\\Desktop\\TG GUI Flask\\static\\img"
+        # delete existing images in folder
+        for filename in os.listdir(img_folder):
+            file_path = os.path.join(img_folder, filename)
+            os.unlink(file_path)
+
+        fig = plt.figure(1)
+        fig.clf()
+        plt.title('Graph')
+
+        pos = nx.spring_layout(graph)
+        edge_labels = nx.get_edge_attributes(graph, 'weight')
+        nx.draw(graph, pos, with_labels=True, node_color='lightblue',
+                node_size=500, font_size=10, font_weight='bold')
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
+        plt.ion()
+        plt.show()
+        plt.savefig(os.path.join(img_folder, 'graph.png'), format='png')
+        return render_template('DisplayGraph.html')
+    else:
+        return render_template('DisplayGraph.html')
+
+
+@app.route('/graph', methods=['GET', 'POST'])
+def graph():
     if request.method == 'POST':
         num_nodes = int(request.form['num_nodes'])
         adj_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
@@ -79,7 +117,7 @@ def index():
 
         return render_template('result.html', bfs_result=bfs_result, dfs_result=dfs_result, prim_result=prim_result)
     else:
-        return render_template('index.html')
+        return render_template('graph.html')
 
 
 if __name__ == '__main__':
